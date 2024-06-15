dd9b5b1914cc38e11149d8c60c4e906d4825c9a3
kevin-arock <kevinstandlee2002@gmail.com>
2024-06-15 17:50:20 +0530

    Add Styles to the keys and add sound functionality when a key is pressed

---
+++ index.js
@@ -0,0 +1,20 @@
+function play(event){
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+window.addEventListener('keydown', play);
+
+function remove(e){
+    if(e.propertyName !== 'transform' ) return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key)=>{
+    key.addEventListener('transitionend',remove)
+});
